260d61a41717d39bb2b3587d3e6b0d0032966697
Aten <jincxmain@gmail.com>
2020-10-05 17:45:17 +0800

    some tips

--- executor/src/lib.rs
+++ executor/src/lib.rs
@@ -1,8 +1,7 @@
 use sc_executor::native_executor_instance;
 pub use sc_executor::NativeExecutor;
 
-// Declare an instance of the native executor named `Executor`. Include the wasm binary as the
-// equivalent wasm code.
+// Declare an instance of the native executor named `Executor`. Not not has wasm part.
 native_executor_instance!(
 	pub Executor,
 	europa_runtime::api::dispatch,
