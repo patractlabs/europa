6d61b354c9f6a212e34db2f7d722cba87b892215
KhepriJin <jincxmain@outlook.com>
2020-10-15 11:33:00 +0800

    add local-executor (#8)

--- client/service/src/client/mod.rs
+++ client/service/src/client/mod.rs
@@ -1,3 +1,24 @@
+// This file is part of europa which is forked form Substrate.
+
+// Copyright (C) 2017-2020 Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// Copyright 2020-2021 patract labs. Licensed under GPL-3.0.
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+mod call_executor;
 mod genesis;
 
 use codec::{Decode, Encode};
