04f395b426ae5dd5d453e7753e641ca9ca59a3fd
Aten <jincxmain@gmail.com>
2020-11-09 21:56:29 +0800

    move SharedParams to local and remove --dev

--- client/cli/src/params/mod.rs
+++ client/cli/src/params/mod.rs
@@ -1,5 +1,7 @@
 mod import_params;
 mod pruning_params;
+mod shared_params;
 
 pub use import_params::*;
 pub use pruning_params::*;
+pub use shared_params::*;
---
+++ client/cli/src/params/shared_params.rs
@@ -0,0 +1,63 @@
+// This file is part of europa which is forked form Substrate.
+
+// Copyright (C) 2018-2020 Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// Copyright 2020-2021 patract labs. Licensed under GPL-3.0.
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+use ec_service::config::BasePath;
+
+/// Shared parameters used by all `CoreParams`.
+#[derive(Debug, StructOpt)]
+pub struct SharedParams {
+	/// Specify the chain specification (one of dev, local, or staging).
+	#[structopt(long, value_name = "CHAIN_SPEC")]
+	pub chain: Option<String>,
+
+	/// Specify custom base path.
+	#[structopt(long, short = "d", value_name = "PATH", parse(from_os_str))]
+	pub base_path: Option<PathBuf>,
+
+	/// Sets a custom logging filter. Syntax is <target>=<level>, e.g. -lsync=debug.
+	///
+	/// Log levels (least to most verbose) are error, warn, info, debug, and trace.
+	/// By default, all targets log `info`. The global log level can be set with -l<level>.
+	#[structopt(short = "l", long, value_name = "LOG_PATTERN")]
+	pub log: Vec<String>,
+}
+
+impl SharedParams {
+	/// Specify custom base path.
+	pub fn base_path(&self) -> Option<BasePath> {
+		self.base_path.clone().map(Into::into)
+	}
+
+	/// Get the chain spec for the parameters provided
+	pub fn chain_id(&self) -> String {
+		match self.chain {
+			Some(ref chain) => chain.clone(),
+			None => "".into(),
+		}
+	}
+
+	/// Get the filters for the logging
+	pub fn log_filters(&self) -> &[String] {
+		&self.log
+	}
+}
