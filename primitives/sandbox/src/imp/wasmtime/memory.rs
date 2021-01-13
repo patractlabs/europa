79e6e6a8814acc881fc782dabb74f181082811cf
clearloop <udtrokia@gmail.com>
2021-01-13 17:02:01 +0800

    feat(sandbox/wasmtime): extract memory operation from substrate/client/executor/wasmtime

--- primitives/sandbox/src/imp/wasmtime/memory.rs
+++ primitives/sandbox/src/imp/wasmtime/memory.rs
@@ -1,32 +1,70 @@
 //! Wasmtime memory
 use crate::Error;
-use sp_std::slice;
+use sp_std::{ops::Range, slice};
 use wasmtime::{Limits, Memory as MemoryRef, MemoryType, Store};
+
+/// Construct a range from an offset to a data length after the offset.
+/// Returns None if the end of the range would exceed some maximum offset.
+pub fn checked_range(offset: usize, len: usize, max: usize) -> Option<Range<usize>> {
+	let end = offset.checked_add(len)?;
+	if end <= max {
+		Some(offset..end)
+	} else {
+		None
+	}
+}
 
 /// Wasmtime memory
 #[derive(Clone)]
 pub struct Memory {
 	inner: MemoryRef,
-	store: Store,
 }
 
 impl Memory {
 	/// New memory
 	pub fn new(initial: u32, maximum: Option<u32>) -> Result<Memory, Error> {
-		let store = Store::default();
 		Ok(Memory {
-			inner: MemoryRef::new(&store, MemoryType::new(Limits::new(initial, maximum))),
-			store,
+			inner: MemoryRef::new(
+				&Store::default(),
+				MemoryType::new(Limits::new(initial, maximum)),
+			),
 		})
 	}
 
 	pub fn get(&self, ptr: u32, buf: &mut [u8]) -> Result<(), Error> {
-		// self.inner.data_unchecked_mut().copy
-		todo!()
+		// This should be safe since we don't grow up memory while caching this reference and
+		// we give up the reference before returning from this function.
+		let memory = unsafe { self.memory_as_slice() };
+		let range = checked_range(ptr as usize, buf.len(), memory.len())
+			.ok_or_else(|| Error::OutOfBounds)?;
+		buf.copy_from_slice(&memory[range]);
+		Ok(())
 	}
 
 	pub fn set(&self, ptr: u32, buf: &[u8]) -> Result<(), Error> {
-		todo!()
+		let memory = unsafe { self.memory_as_slice_mut() };
+		let range = checked_range(ptr as usize, buf.len(), memory.len())
+			.ok_or_else(|| Error::OutOfBounds)?;
+		&mut memory[range].copy_from_slice(buf);
+		Ok(())
+	}
+
+	/// Returns linear memory of the wasm instance as a slice.
+	///
+	/// # Safety
+	///
+	/// Wasmtime doesn't provide comprehensive documentation about the exact behavior of the data
+	/// pointer. If a dynamic style heap is used the base pointer of the heap can change. Since
+	/// growing, we cannot guarantee the lifetime of the returned slice reference.
+	unsafe fn memory_as_slice(&self) -> &[u8] {
+		let ptr = self.inner.data_ptr() as *const _;
+		let len = self.inner.data_size();
+
+		if len == 0 {
+			&[]
+		} else {
+			slice::from_raw_parts(ptr, len)
+		}
 	}
 
 	/// Returns linear memory of the wasm instance as a slice.
